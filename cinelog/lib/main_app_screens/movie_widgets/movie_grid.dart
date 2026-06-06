7620bd0b5450a40f16f9c7759f5c289b833ae7fd
Afonso Alvané <afo.alv0@gmail.com>
2026-06-06 20:28:11 +0100

    Main Page com scroll continuo

--- cinelog/lib/main_app_screens/movie_widgets/movie_grid.dart
+++ cinelog/lib/main_app_screens/movie_widgets/movie_grid.dart
@@ -16,27 +16,58 @@ class MovieGrid extends StatefulWidget {
 
 class MovieGridState extends State<MovieGrid> {
   List<Movie> movieList = [];
+  final ScrollController _controller = ScrollController();
+  int currentPage = 1;
+  bool isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+
+    _controller.addListener(() async {
+      if(_controller.position.pixels >= _controller.position.maxScrollExtent -150){
+        _loadNextPage();
+      }
+    });
+  }
+
+  void _loadNextPage() async{
+    if (isLoading) return;
+
+    setState(() => isLoading = true);
+    
+    currentPage++;
+    
+    List<Movie> nextPage = await ApiService.getMainPageMovies(page: currentPage);
+
+    setState(() {
+      movieList.addAll(nextPage);
+      isLoading = false;     
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(future: ApiService.getMainPageMovies(), 
       builder: (context, asyncSnapshot){
         if (asyncSnapshot.hasData){
-          movieList = asyncSnapshot.requireData;
-                    
+          if(movieList.isEmpty){ //Para garantir que quando o estado for atualizado a movieList é atualizada com os novos filmes e não resetada pelo FutureBuilder
+            movieList = asyncSnapshot.requireData;
+          }
           return GridView.builder(
-          shrinkWrap: true,
-          physics: widget.neverScrollable ? NeverScrollableScrollPhysics() : null,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, 
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 25,
-          childAspectRatio: 0.75,
-          ),
-          itemCount: movieList.length,
-          itemBuilder: (context, index) {
-            return  MovieCard(movie: movieList[index]);
-          });
+            controller: _controller,
+            shrinkWrap: true,
+            physics: widget.neverScrollable ? NeverScrollableScrollPhysics() : null,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, 
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 25,
+              childAspectRatio: 0.75,
+            ),
+            itemCount: movieList.length,
+            itemBuilder: (context, index) {
+              return  MovieCard(movie: movieList[index]);
+            }
+          );
         }
         return Center(
           child: CircularProgressIndicator(
